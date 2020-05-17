5186dca7c62044734105299e3875d4a7953f831e
Putra Budiman <cristminix@gmail.com>
2020-05-17 16:47:36 +0700

    ForgetPage

--- src/components/ForgetPage.js
+++ src/components/ForgetPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View,StyleSheet, Text, Image, TouchableHighlight, TextInput, KeyboardAvoidingView ,SafeAreaView, ScrollView} from 'react-native';
 import Constants from 'expo-constants';
 import Spinner from 'react-native-loading-spinner-overlay';
+import { LinearGradient } from 'expo-linear-gradient';
+import App from '../../App';
 
 class ForgetPage extends React.Component {
     
@@ -12,12 +14,45 @@ class ForgetPage extends React.Component {
         _ie_placeHolderText : 'Email',
         _ie_lbl_style: {display:'none'},
         _ie_clr_btn_style: {height: 0, width: 0, opacity: 0},
+        _form_errorMessage: '',
+        _form_err_msg_style:{ display:'none'},
     };
 
     goBack=()=>{
         this.props.navigation.navigate('LoginPage');
     };
+    
+    formSubmit = () => {
+        if(this.state.email.length == 0){
+            return;
+        }
+        let validEmail = App.helper.validateEmail(this.state.email);
+        if(!validEmail){
+            this.setState({
+                _form_errorMessage : 'Email tidak valid.',
+                _form_err_msg_style:{ display:'flex'}
+            });
+        }
+    };
+    _onInputEmailFocus = () => {
+        this.setState({ 
+            _ie_Focused: true,
+            _ie_underlineColor: '#009EEE',
+            _ie_placeHolderText: '',
+            _ie_lbl_style: {display:'flex',color:'#009EEE',marginTop:4},
+            _ie_clr_btn_style: {marginTop:30}
 
+        });
+    };
+    _onInputEmailBlur = () => {
+        this.setState({ 
+            _ie_Focused: false,
+            _ie_underlineColor: '#EFEFEF',
+            _ie_placeHolderText : 'Email',
+            _ie_lbl_style: {display:'none'},
+            _ie_clr_btn_style: {height: 0, width: 0, opacity: 0}
+        }); 
+    };
 	render(){
         let icons = {
             back :  require('../../assets/icon/chevron-left.png') 
@@ -25,22 +60,26 @@ class ForgetPage extends React.Component {
 		return (
 			<KeyboardAvoidingView style={styles.wrapper} behavior={Platform.OS === "ios" ? "padding" : null}> 
                 <Spinner visible={this.state.spinner} textContent={'Memuat data...'} textStyle={styles.spinnerTextStyle}/>
-                <View style={styles.header}>
-                    <View style={{paddingHorizontal:10,paddingVertical:20}}>
+                <View style={{}}>
+                <LinearGradient colors={['#009EEE', '#00A4F6']} start={[0.0, 0.101]} style={[{paddingVertical:20},styles.headerGradient]}>
+
+                    <View style={{paddingHorizontal:10,paddingVertical:0}}>
                     <TouchableHighlight underlayColor='transparent' onPress={()=>{this.goBack()}} >
                         <Image style={{width:22}} source={icons.back}/>
                     </TouchableHighlight>
                     </View>
-                    <View style={{flex:1,alignItems:'center',paddingVertical:20}}>
-                        <Text style={{color:'#ffffff',fontSize:14,marginLeft:-22}}>Lupa Sandi</Text>
+                    <View style={{flex:1,alignItems:'center',paddingVertical:0}}>
+                        <Text style={{color:'#ffffff',fontSize:14,marginLeft:-40,marginTop:-20}}>Lupa Sandi</Text>
                     </View>
+                    </LinearGradient>
                 </View>
-                <SafeAreaView style={styles.content}>
+                <SafeAreaView style={[styles.content]}>
                     <ScrollView style={{padding:5}}>
                     
-                        <Text style={{fontWeight:'bold',fontSize:16,color:'#3A3E4A'}}>Kesulitan Mengakses akun Anda?</Text>
-                        <Text style={{marginVertical:10,fontSize:12,color:'#8F8EA0',lineHeight:16}}>Lupa kata sandi? masukkan email login Anda dibawah ini. Kami akan mengirimkan pesan email beserta tautan untuk reset kata sandi Anda.</Text>
-                        <View style={styles.form}>
+                        <View style={[styles.form]}>
+                            <Text style={{fontWeight:'bold',fontSize:16,color:'#3A3E4A'}}>Kesulitan Mengakses akun Anda?</Text>
+                            <Text style={{marginVertical:10,fontSize:12,color:'#8F8EA0',lineHeight:16}}>Lupa kata sandi? masukkan email login Anda dibawah ini. Kami akan mengirimkan pesan email beserta tautan untuk reset kata sandi Anda.</Text>
+                        
                             <View style={styles.formGroup}>
                                 <Text style={[{paddingTop:5,paddingLeft:5},this.state._ie_lbl_style]}>Email</Text>
 
@@ -59,14 +98,45 @@ class ForgetPage extends React.Component {
                                     <Image  source={icons.close}/>
                                 </TouchableHighlight>       
                             </View>
-                        </View>    
+                            <View style={styles.formGroup}>
+                                <View  style={[styles.errorMessage,this.state._form_err_msg_style]}>
+                                    <Text style={{color:'#ffffff'}}>{this.state._form_errorMessage}</Text>
+                                </View>
+                            </View>
+                        </View>
+                            
                     </ScrollView>
+                    <View style={{flex:1,flexDirection:'column-reverse',padding:5}}>
+                            <TouchableHighlight underlayColor='transparent' onPress={()=>{ this.formSubmit() }} >
+                                <View>
+                                    <LinearGradient colors={['#009EEE', '#00A4F6']} start={[0.0, 0.101]} 
+                                     style={{flex:1,alignItems:'center',padding:20,borderRadius:10}}>
+                                        <Text style={[{marginTop:-10},styles.btnActionText]}>Ubah Kata Sandi</Text>
+
+                                    </LinearGradient>    
+                                </View>
+                            </TouchableHighlight>
+                        </View>
                 </SafeAreaView>
             </KeyboardAvoidingView>    
 		);
 	}
 }
 const styles = StyleSheet.create({
+    errorMessage:{
+        backgroundColor:'#e22134',
+        marginVertical:15,
+        alignItems:'center',
+        padding:5,
+        borderRadius:5,
+        marginHorizontal:20
+    },
+    buttonGroup:{
+
+    },
+    headerGradient:{
+       
+    },
     formIcon:{
         position:'absolute',
         marginTop:20,
@@ -108,17 +178,16 @@ const styles = StyleSheet.create({
     formGroup:{
         paddingVertical:2
     },
-    btnLogin:{
+    btnAction:{
         backgroundColor:'#CACACC',
         borderRadius:50,
         padding:12,
         marginTop:10
     },
-    btnLoginText:{
+    btnActionText:{
         color:'#fff',
         fontSize:14,
-        fontWeight:'bold',
-        textAlign:'center'
+        fontWeight:'bold'
     },
   
   
