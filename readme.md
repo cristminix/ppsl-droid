9e6df6bcde9964955caa2945f65758c38864b0c2
Putra Budiman <cristminix@gmail.com>
2020-05-17 15:50:16 +0700

    Init forget page

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 # -- STEP --
 
 
-- shell>git clone https://github.com/cristminix/ppsl-droid.git
-- shell>cd ppsl-droid
-- shell@ppsl-droid>rm package-lock.json
-- shell@ppsl-droid>del package-lock.json
-- shell@ppsl-droid>yarn
-- shell@ppsl-droid>yarn start
+- git clone https://github.com/cristminix/ppsl-droid.git
+- cd ppsl-droid
+- rm package-lock.json
+- rm yarn.lock
+- yarn
+- yarn start
+- a
--- src/components/ForgetPage.js
+++ src/components/ForgetPage.js
@@ -4,183 +4,69 @@ import Constants from 'expo-constants';
 import Spinner from 'react-native-loading-spinner-overlay';
 
 class ForgetPage extends React.Component {
+    
+    state = {
+        spinner:false,
+        email:'',
+        _ie_underlineColor  : '#EFEFEF',
+        _ie_placeHolderText : 'Email',
+        _ie_lbl_style: {display:'none'},
+        _ie_clr_btn_style: {height: 0, width: 0, opacity: 0},
+    };
+
     goBack=()=>{
         this.props.navigation.navigate('LoginPage');
     };
-    state = {
-        spinner:false
-    };
+
 	render(){
+        let icons = {
+            back :  require('../../assets/icon/chevron-left.png') 
+        };
 		return (
-			<KeyboardAvoidingView style={styles.wrapper} behavior={Platform.OS === "ios" ? "padding" : null}>
-                
-                <Spinner visible={this.state.spinner} textContent={'Loading...'} textStyle={styles.spinnerTextStyle}/>
+			<KeyboardAvoidingView style={styles.wrapper} behavior={Platform.OS === "ios" ? "padding" : null}> 
+                <Spinner visible={this.state.spinner} textContent={'Memuat data...'} textStyle={styles.spinnerTextStyle}/>
                 <View style={styles.header}>
                     <View style={{paddingHorizontal:10,paddingVertical:20}}>
-                    <TouchableHighlight onPress={()=>{this.goBack()}} >
-                    <Image style={{width:22}} source={ require('../../assets/icon/chevron-left.png') }/>
-                        
+                    <TouchableHighlight underlayColor='transparent' onPress={()=>{this.goBack()}} >
+                        <Image style={{width:22}} source={icons.back}/>
                     </TouchableHighlight>
                     </View>
-                    <View style={{flex:1,paddingLeft:120,paddingVertical:20}}>
-                        <Text style={{color:'#ffffff',fontSize:14}}>Lupa Sandi</Text>
+                    <View style={{flex:1,alignItems:'center',paddingVertical:20}}>
+                        <Text style={{color:'#ffffff',fontSize:14,marginLeft:-22}}>Lupa Sandi</Text>
                     </View>
                 </View>
                 <SafeAreaView style={styles.content}>
-                <ScrollView style={{padding:20}}>
-                
-                    <Text style={{fontWeight:'bold',fontSize:16}}>Lupa Kata Sandi ?</Text>
-                    <Text style={{marginVertical:10}}>Silahkan masukkan alamat Email Anda</Text>
+                    <ScrollView style={{padding:5}}>
                     
-                </ScrollView>
+                        <Text style={{fontWeight:'bold',fontSize:16,color:'#3A3E4A'}}>Kesulitan Mengakses akun Anda?</Text>
+                        <Text style={{marginVertical:10,fontSize:12,color:'#8F8EA0',lineHeight:16}}>Lupa kata sandi? masukkan email login Anda dibawah ini. Kami akan mengirimkan pesan email beserta tautan untuk reset kata sandi Anda.</Text>
+                        <View style={styles.form}>
+                            <View style={styles.formGroup}>
+                                <Text style={[{paddingTop:5,paddingLeft:5},this.state._ie_lbl_style]}>Email</Text>
+
+                                <TextInput style={styles.textInput}
+                                value={this.state.email}
+                                onChangeText={( email ) => this.setState({ email })}
+                                onKeyPress={this._validateInput}
+                                placeholder={this.state._ie_placeHolderText}
+                                onFocus={this._onInputEmailFocus}
+                                onBlur={this._onInputEmailBlur}
+                                placeholderTextColor="#8F8EA0"
+                                autoCapitalize = 'none'
+                                underlineColorAndroid={this.state._ie_underlineColor}
+                                    />
+                                <TouchableHighlight underlayColor='transparent' onPress={this._ie_clear} style={[styles.formIconClose,this.state._ie_clr_btn_style]}>
+                                    <Image  source={icons.close}/>
+                                </TouchableHighlight>       
+                            </View>
+                        </View>    
+                    </ScrollView>
                 </SafeAreaView>
             </KeyboardAvoidingView>    
 		);
 	}
 }
 const styles = StyleSheet.create({
-    imagePreview:{
-        width:100,
-        height:100,
-        marginBottom:5
-    },
-    wrapper:{
-        paddingTop:Constants.statusBarHeight,
-        flex:1,
-        backgroundColor:'#F8F7FC' 
-    },
-    header:{
-        // flex:1,
-        flexDirection:'row',
-        backgroundColor:'#00A4F6',
-        // color:'#FFF'
-    },
-    content:{
-        flex:2,
-        backgroundColor:'white',
-        borderTopLeftRadius:25,
-        borderTopRightRadius: 25,
-        padding:20
-    },
-    photoUploadWrp:{
-        backgroundColor:'white',
-        borderRadius:5,
-        borderColor:'#EFEFEF',
-        borderWidth:1,
-        padding:20,
-        marginTop:20,
-        marginBottom:10,
-        justifyContent:'center',
-        alignItems:'center',
-        flex:1
-    },
-    headerTitle:{
-        flex:1,
-        color:'#36227C',
-        fontWeight:'bold',
-        fontSize:20
-    },
-    logo:{
-        flex:2,
-        resizeMode:'contain',
-        marginTop:10
-    },
-    content:{
-        flex:2,
-        backgroundColor:'white',
-        borderTopLeftRadius:25,
-        borderTopRightRadius: 25,
-        padding:20,
-        paddingBottom:0
-    },
-    defaultText:{
-        // fontSize:12,
-        // letterSpacing:-0.02
-    },
-    anchor:{
-        color:'#009EEE',
-        textDecorationLine:'underline'
-    },
-    anchorRight:{
-        color:'#009EEE',
-        textDecorationLine:'underline',
-        textAlign:'right'
-    },
-    anchorCenter:{
-        color:'#009EEE',
-        textDecorationLine:'underline',
-        textAlign:'center'
-    },
-    anchorBold:{
-        color:'#009EEE',
-        fontWeight:'bold'
-    },
-    welcomeText:{
-        color:'#3A3E4A',
-        fontWeight:'bold',
-        fontSize:16,
-    },  
-    info:{
-        paddingVertical:20,
-        flexDirection:'row',
-        justifyContent:'center'
-    },
-    help:{
-        // paddingVertical:8,
-        flexDirection:'row',
-        justifyContent:'center'
-    },
-    formGroup:{
-        paddingVertical:2
-    },
-    btnLogin:{
-        backgroundColor:'#CACACC',
-        borderRadius:50,
-        padding:12,
-        marginTop:10
-    },
-    btnLoginText:{
-        color:'#fff',
-        fontSize:14,
-        fontWeight:'bold',
-        textAlign:'center'
-    },
-    btnRegister:{
-        backgroundColor:'#CACACC',
-        borderRadius:50,
-        padding:12,
-        marginTop:10,
-        marginBottom:40
-    },
-    btnRegisterText:{
-        color:'#fff',
-        fontSize:14,
-        fontWeight:'bold',
-        textAlign:'center'
-    },
-    btnUpload:{
-        borderColor:'#009EEE',
-        borderWidth:1,
-        borderRadius:5,
-        padding:12,
-        marginTop:10
-
-    },
-    btnUploadText:{
-        color:'#009EEE',
-        fontSize:14,
-        fontWeight:'bold',
-        textAlign:'center'
-    },
-    textInput:{
-        paddingVertical:10,
-        paddingHorizontal:5,
-        fontSize:14
-    },
-    inlineIcon:{
-        position:'relative',
-        margin:4
-    },
     formIcon:{
         position:'absolute',
         marginTop:20,
@@ -196,25 +82,46 @@ const styles = StyleSheet.create({
         padding:10
 
     },
-    formIconViewPass:{
-        position:'absolute',
-        marginTop:0,
-        right:0,
-        
-        width:40,
-        height:40,
-        padding:10
+    textInput:{
+        paddingVertical:10,
+        paddingHorizontal:5,
+        fontSize:14
     },
-    formIconHidePass:{
-        position:'absolute',
-        marginTop:0,
-        right:0,
-     
-        width:40,
-        height:40,
-        padding:10
+    spinnerTextStyle:{
 
     },
+    wrapper:{
+        paddingTop:Constants.statusBarHeight,
+        flex:1,
+        backgroundColor:'#F8F7FC' 
+    },
+    header:{
+        flexDirection:'row',
+        backgroundColor:'#00A4F6',
+    },
+    content:{
+        flex:1,
+        backgroundColor:'white',
+        padding:20
+    },
+ 
+    formGroup:{
+        paddingVertical:2
+    },
+    btnLogin:{
+        backgroundColor:'#CACACC',
+        borderRadius:50,
+        padding:12,
+        marginTop:10
+    },
+    btnLoginText:{
+        color:'#fff',
+        fontSize:14,
+        fontWeight:'bold',
+        textAlign:'center'
+    },
+  
+  
     errorMessage:{
         backgroundColor:'#e22134',
         marginVertical:15,
